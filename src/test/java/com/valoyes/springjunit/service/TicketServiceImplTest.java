f5a5d37b955a903ded0edc36c279b9eadc57928a
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-05-06 15:50:13 +0200

    configure ans set our TU

--- src/test/java/com/valoyes/springjunit/service/TicketServiceImplTest.java
+++ src/test/java/com/valoyes/springjunit/service/TicketServiceImplTest.java
@@ -4,12 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.mockito.Mockito.*;
 
 import com.valoyes.springjunit.dao.TicketDAO;
+import com.valoyes.springjunit.dto.Ticket;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:application-context.xml")
 public class TicketServiceImplTest {
 
 	private static final int EXPECTED_RESULTADO = 1;
@@ -20,6 +28,7 @@ public class TicketServiceImplTest {
 	TicketDAO dao;
 	
 	@Autowired
+	@InjectMocks	// permite inyectar todas las dependencies mockeadas a este objeto
 	private TicketServiceImpl service;
 	
 	@Before
@@ -31,6 +40,7 @@ public class TicketServiceImplTest {
 	public void buyTickets_ShouldReturnAValidValue() {
 		
 		// when
+		when(dao.createTicket(any(Ticket.class))).thenReturn(1);
 		int resultado = service.buyTicket(PASSENGER_NAME, PASSENGER_PHONE);
 		
 		// then
